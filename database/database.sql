1ad9dca3d4c12dbcf39d18ba4762f06723d41f42
clementBernard130 <clement.bernard.pro130@gmail.com>
2025-03-27 13:09:16 +0100

    Permet d'ajouter un processus sans l'obligation de se connecter

--- database/database.sql
+++ database/database.sql
@@ -10,16 +10,21 @@ CREATE TABLE IF NOT EXISTS Processus (
 CREATE TABLE IF NOT EXISTS Etapes (
     idEtapes INT AUTO_INCREMENT PRIMARY KEY,
     idProcessus INT NOT NULL,
+    idBac INT,
     descriptionEtape TEXT NOT NULL,
     FOREIGN KEY (idProcessus) REFERENCES Processus(idProcessus) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Image (
     idImage INT AUTO_INCREMENT PRIMARY KEY,
-    idEtapes INT NOT NULL,
     nomFichier VARCHAR(255) NOT NULL,
     typeMIME VARCHAR(50) NOT NULL,
-    contenuBlob MEDIUMBLOB NOT NULL, -- 16Mo
+    contenuBlob MEDIUMBLOB NOT NULL,  -- 16 Mo max pour le contenu de l'image
     tailleImage INT UNSIGNED NOT NULL,
-    FOREIGN KEY (idEtapes) REFERENCES Etapes(idEtapes) ON DELETE CASCADE
+    idProcessus INT DEFAULT NULL,
+    idEtapes INT DEFAULT NULL,
+    typeImage ENUM('processus', 'etape') NOT NULL,
+    FOREIGN KEY (idProcessus) REFERENCES Processus(idProcessus) ON DELETE CASCADE,
+    FOREIGN KEY (idEtapes) REFERENCES Etapes(idEtapes) ON DELETE CASCADE,
+    CHECK ( (idProcessus IS NOT NULL AND idEtapes IS NULL) OR (idProcessus IS NULL AND idEtapes IS NOT NULL) )
 );
