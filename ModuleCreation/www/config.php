9f0a3cd480de2719be9a243bd7840c6333cdafd0
clementBernard130 <clement.bernard.pro130@gmail.com>
2025-03-27 14:21:06 +0100

    Modification config

--- ModuleCreation/www/config.php
+++ ModuleCreation/www/config.php
@@ -4,14 +4,14 @@
 define("DEBUG", false);
 
 // Pour la base de données
-define("DB_DRIVER", false); // true pour MySQL, false sans base de données
+define("DB_DRIVER", true); // true pour MySQL, false sans base de données
 define("DB_HOST", "localhost");
 define("DB_USER", "ergoWork");
 define("DB_PASS", "password");
 define("DB_NAME", "ergonomic_workstation");
 
 // URL
-define("ROOT_PATH", dirname($_SERVER['SCRIPT_FILENAME']));
+define("ROOT_PATH", "/");
 //define("URL_PATH", "http://" . $_SERVER['HTTP_HOST'] . "/ergonomic-workstation/");
 define("URL_PATH", "http://" . $_SERVER['HTTP_HOST'] . "/");
 
