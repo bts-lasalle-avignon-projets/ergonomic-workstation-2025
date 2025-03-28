c3a9824f58a81486e6baedca1d6ed4588c92a893
tvaira <tvaira@free.fr>
2025-03-28 14:26:30 +0100

    Ajoute docker-compose pour MySQL

--- ModuleCreation/www/config.php
+++ ModuleCreation/www/config.php
@@ -5,8 +5,9 @@ define("DEBUG", false);
 
 // Pour la base de données
 define("DB_DRIVER", true); // true pour MySQL, false sans base de données
-define("DB_HOST", "localhost");
+define("DB_HOST", "127.0.0.1");
 define("DB_USER", "ergoWork");
+//define("DB_USER", "root");
 define("DB_PASS", "password");
 define("DB_NAME", "ergonomic_workstation");
 
