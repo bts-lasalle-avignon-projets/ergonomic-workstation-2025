0fc18152dca712c2cffd83fa9a819087bb3aecf1
tvaira <tvaira@free.fr>
2025-03-30 09:57:56 +0200

    Remanie docker-compose

--- ModuleCreation/www/config.php
+++ ModuleCreation/www/config.php
@@ -10,7 +10,6 @@ define("NO_LOGIN", true);
 define("DB_DRIVER", true); // true pour MySQL, false sans base de données
 define("DB_HOST", "127.0.0.1");
 define("DB_USER", "ergoWork");
-//define("DB_USER", "root");
 define("DB_PASS", "password");
 define("DB_NAME", "ergonomic_workstation");
 
