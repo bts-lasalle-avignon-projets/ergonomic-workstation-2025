7a1b86b58829bea1e302f435fd8d720ba667cd61
clementBernard130 <clement.bernard.pro130@gmail.com>
2025-05-14 16:21:33 +0200

    voir etape

--- ModuleCreation/www/views/Processus/view.php
+++ ModuleCreation/www/views/Processus/view.php
@@ -1,6 +1,26 @@
-<div class="card col col-lg-5 mx-3 mx-lg-auto p-0">
-	<div class="card-header">
-		<h3 class="card-title">Voir les étapes d'un processus</h3>
-	</div>
+<div class="container d-flex flex-column align-items-center">
+	<?php foreach ($datas as $etape) : ?>
+		<div class="card col-12 col-lg-6" style="margin-bottom: 20px; max-height: 600px;">
+			<div class="card-header">
+				<h3 class="card-title">Étape n° <?php echo htmlspecialchars($etape['numeroEtape']) ?></h3>
+			</div>
+			<div class="card-body">
+				<h5>Description Étape</h5>
+				<p><?php echo htmlspecialchars($etape['descriptionEtape']); ?></p>
 
+				<h5>Bac</h5>
+				<p><?php echo htmlspecialchars($etape['bac']); ?></p>
+
+				<h5>Image</h5>
+				<?php if (!empty($etape['image']['contenuBlob'])) : ?>
+					<img src="data:<?php echo htmlspecialchars($etape['image']['typeMIME']); ?>;base64,<?php echo base64_encode($etape['image']['contenuBlob']); ?>" 
+						alt="Image du processus" 
+						class="img-fluid" 
+						style="max-width: 100%; max-height: 300px; object-fit: contain;" />
+				<?php else : ?>
+					<p class="mt-3">Aucune image disponible.</p>
+				<?php endif; ?>
+			</div>
+		</div>
+	<?php endforeach; ?>
 </div>
