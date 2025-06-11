455bffe250ebdd7d0227fd4bda84680c4ff11f92
Clement <clement.bernard.pro130@gmail.com>
2025-06-11 11:23:59 +0200

    message d'alerte si pas de stat

--- ModuleCreation/www/views/Processus/statistique.php
+++ ModuleCreation/www/views/Processus/statistique.php
@@ -1,27 +1,34 @@
-<div class="col-12">
-    <div class="card-header">
-        <h3 class="card-title">Statistique du processus : <?php echo htmlspecialchars($datas[0]['nomProcessus']); ?></h3>
-    </div>
-    <div class="card mb-4">
-        <div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID de l'assemblage</th>
-                        <th>Nombre de mauvaise pioche</th>
-                        <th>Durée</th>
-                    </tr>
-                </thead>
-                <tbody id="table-processus">
-                    <?php foreach ($datas as $AssemblageData) : ?>
+<?php if (!empty($datas)) : ?>
+    <div class="col-12">
+        <div class="card-header">
+            <h3 class="card-title">
+                Statistique du processus : 
+                <?php echo htmlspecialchars($datas[0]['nomProcessus']); ?>
+            </h3>
+        </div>
+        <div class="card mb-4">
+            <div>
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td><?php echo htmlspecialchars($AssemblageData['idAssemblage']); ?></td>
-                            <td><?php echo htmlspecialchars($AssemblageData['tauxErreur']); ?></td>
-                            <td><?php echo htmlspecialchars($AssemblageData['dureeProcessus']); ?></td>
+                            <th>ID de l'assemblage</th>
+                            <th>Nombre de mauvaise pioche</th>
+                            <th>Durée</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody id="table-processus">
+                        <?php foreach ($datas as $AssemblageData) : ?>
+                            <tr>
+                                <td><?php echo htmlspecialchars($AssemblageData['idAssemblage']); ?></td>
+                                <td><?php echo htmlspecialchars($AssemblageData['tauxErreur']); ?></td>
+                                <td><?php echo htmlspecialchars($AssemblageData['dureeProcessus']); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
+<?php else : ?>
+    <div class="alert alert-info">Aucune donnée à afficher pour ce processus.</div>
+<?php endif; ?>
